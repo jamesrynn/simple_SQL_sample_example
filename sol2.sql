81dc4a3eb6b3f6c99ff57f5970a56961c51ae2ea
James <jajrynn@googlemail.com>
2020-04-01 14:02:52 +0100

    began sol2

--- sol2.sql
+++ sol2.sql
@@ -4,39 +4,52 @@ Solutions to Part 2 of the Query Tasks.
 
 
 -- Q5: Which portfolio are people most likely to pay into?
+SELECT		A.PortfolioId, COUNT(C.TransactionAmount) AS NumPayments
+FROM 		Account AS A
+			INNER JOIN Collections AS C ON A.AccountId = C.AccountId 
+WHERE 		C.TransactionAmount>0
+GROUP BY 	A.PortfolioID
+ORDER BY	NumPayments DESC
+
+SELECT PortfolioID 
+
+
+-- Get Account Ids with Positive transaction amounts
+(
+SELECT 		A.AccountId, A.PortfolioId, SUM(C.TransactionAmount) AS TotalPayed
+FROM 		Account AS A
+			INNER JOIN Collections 	AS C ON A.AccountId = C.AccountId
+GROUP BY	A.AccountId, A.PortfolioId 
+HAVING		SUM(C.TransactionAmount)>0
+ORDER BY 	A.AccountId
+) AS M 
+
+
+-- Get Account Ids with 0 total transaction amounts
+(
+SELECT		A.AccountId, SUM(C.TransactionAmount) AS TotalPayed
+FROM 		Account AS A
+			INNER JOIN Collections AS C ON A.AccountId = C.AccountId
+GROUP BY 	A.AccountId
+HAVING		SUM(C.TransactionAmount)=0
+			OR
+			ISNULL(1,C.TransactionAmount)=1
+ORDER BY 	A.AccountId
+) AS N
 
 
 
-
+INNER JOIN Portfolio 	AS P ON A.PortfolioId = P.PortfolioId
 -- Q6: Which product type are people most likely to pay into?
 
 
 
-/*
-Q7: What is the pay rate over the last 12 months? That is the number of customers who have paid in the last 12 months divided by the number of customers who 
-had not cleared their balance in the last 12 months.
-*/
+-- Q7: What is the pay rate over the last 12 months? That is the number of customers who have paid in the last 12 months divided by the number of customers who 
+--had not cleared their balance in the last 12 months.
 
 
--- Q4: Which customer has the highest number of accounts?
--- Solution 1: Count the number of accounts each customer has and list them in decreasing order. Read the answer from the top of the table that is returned.
-SELECT 		TOP 5 C.FirstName, C.LastName, AC.CustomerId, COUNT(AC.AccountId) AS NumAcc
-FROM 		AccountCustomer AS AC
-			INNER JOIN Customer AS C ON AC.CustomerId = C.CustomerId 
-GROUP BY 	C.FirstName, C.LastName, AC.CustomerId
-ORDER BY 	COUNT(AC.AccountId) DESC
 
-	
--- Solution 2: Count the number of accounts each customer has using a subquery and then retrievefrom that the maximum value of the results returned.
--- Note that here we do not return the details of the customer/customers who has/have this number of accounts.
-SELECT 	MAX(S.NumAcc) AS MaxNAccounts
-FROM 	(
-		SELECT 		C.FirstName, C.LastName, AC.CustomerId, COUNT(AC.AccountId) AS NumAcc
-		FROM 		AccountCustomer AS AC
-					INNER JOIN Customer AS C ON AC.CustomerId = C.CustomerId 
-		GROUP BY 	C.FirstName, C.LastName, AC.CustomerId) AS S
 
-	
 
 				
 
