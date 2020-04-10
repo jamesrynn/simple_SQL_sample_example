1989ce7af4125e08e59179fa5a758c7c50fccf25
James <jajrynn@googlemail.com>
2020-04-10 20:23:34 +0100

    reformatted sol1 and sol2

--- sol1.sql
+++ sol1.sql
@@ -4,51 +4,84 @@ Solutions to Part 1 of the Query Tasks.
 
 
 -- Q1: What is the average balance in each portfolio?
-SELECT 		P.PortfolioName, AVG(A.PurchaseBalance) 
+SELECT 		P.PortfolioName,
+			AVG(A.PurchaseBalance) 
+			
 FROM 		Account AS A
 			INNER JOIN 	Portfolio as P ON P.PortfolioId = A.PortfolioId 
+			
 GROUP BY 	P.PortfolioName
+
 ORDER BY 	P.PortfolioName
 
 
 
 -- Q2: What is the average balance by Product Type?
-SELECT A.ProductType, AVG(A.PurchaseBalance) 
-FROM Account AS A
-GROUP BY A.ProductType 
-ORDER BY A.ProductType
+SELECT 		A.ProductType,
+			AVG(A.PurchaseBalance) 
+			
+FROM 		Account AS A
+
+GROUP BY 	A.ProductType 
+
+ORDER BY 	A.ProductType
 
 
 
 -- Q3: What is the average balance today (hint: discount previous collections from purchase value) of all customers whose surname begins with a G?
-SELECT DISTINCT 	Cu.LastName, Cu.FirstName, AVG(A.PurchaseBalance - Co.TransactionAmount)
+SELECT DISTINCT 	Cu.LastName,
+					Cu.FirstName,
+					AVG(A.PurchaseBalance - Co.TransactionAmount) AS AverageBalance
+					
 FROM 				Account as A
 					INNER JOIN Collections 		AS Co ON Co.AccountId = A.AccountId
 					INNER JOIN AccountCustomer 	AS AC ON AC.AccountId = A.AccountId
 					INNER JOIN Customer 		AS Cu ON Cu.CustomerId = AC.CustomerId
-WHERE 				SUBSTRING(Cu.LastName,1,1)='G'
-GROUP BY 			Cu.LastName, Cu.FirstName
-ORDER BY 			Cu.LastName, Cu.FirstName
+					
+WHERE				Cu.LastName LIKE 'G%'
+--WHERE 			SUBSTRING(Cu.LastName,1,1)='G'
+
+GROUP BY 			Cu.LastName,
+					Cu.FirstName
+					
+ORDER BY 			Cu.LastName,
+					Cu.FirstName
 
 
 
 -- Q4: Which customer has the highest number of accounts?
 -- Solution 1: Count the number of accounts each customer has and list them in decreasing order. Read the answer from the top of the table that is returned.
-SELECT 		TOP 5 C.FirstName, C.LastName, AC.CustomerId, COUNT(AC.AccountId) AS NumAcc
+SELECT 		TOP 5
+			C.FirstName,
+			C.LastName,
+			AC.CustomerId,
+			COUNT(AC.AccountId) AS NumAcc
+			
 FROM 		AccountCustomer AS AC
 			INNER JOIN Customer AS C ON AC.CustomerId = C.CustomerId 
+
 GROUP BY 	C.FirstName, C.LastName, AC.CustomerId
+
 ORDER BY 	COUNT(AC.AccountId) DESC
 
 	
 -- Solution 2: Count the number of accounts each customer has using a subquery and then retrievefrom that the maximum value of the results returned.
 -- Note that here we do not return the details of the customer/customers who has/have this number of accounts.
 SELECT 	MAX(S.NumAcc) AS MaxNAccounts
+
 FROM 	(
-		SELECT 		C.FirstName, C.LastName, AC.CustomerId, COUNT(AC.AccountId) AS NumAcc
+		SELECT 		C.FirstName,
+					C.LastName
+					AC.CustomerId,
+					COUNT(AC.AccountId) AS NumAcc
+					
 		FROM 		AccountCustomer AS AC
 					INNER JOIN Customer AS C ON AC.CustomerId = C.CustomerId 
-		GROUP BY 	C.FirstName, C.LastName, AC.CustomerId) AS S
+					
+		GROUP BY 	C.FirstName,
+					C.LastName,
+					AC.CustomerId
+		) AS S
 
 	
 
--- sol2.sql
+++ sol2.sql
@@ -4,48 +4,44 @@ Solutions to Part 2 of the Query Tasks.
 
 
 -- Q5: Which portfolio are people most likely to pay into?
-SELECT		A.PortfolioId, COUNT(C.TransactionAmount) AS NumPayments
+-- i.e., Number of people who have ever paid / number of people in portfolio.
+SELECT 		A.PortfolioId,
+			COUNT(A.AccountId) AS NumAccounts,
+			AVG(
+				CASE WHEN S.AccountId IS NOT NULL 
+       			THEN 1.0
+       			ELSE 0.0
+				END
+				) AS PayRate
+				
 FROM 		Account AS A
-			INNER JOIN Collections AS C ON A.AccountId = C.AccountId 
-WHERE 		C.TransactionAmount>0
-GROUP BY 	A.PortfolioID
-ORDER BY	NumPayments DESC
+			-- Accounts where payments have been made.
+			LEFT JOIN	(
+						SELECT 		A.AccountId,
+									A.PortfolioId
+									
+						FROM 		Account AS A
+									INNER JOIN Collections AS C ON A.AccountId = C.AccountId
+									
+						GROUP BY	A.AccountId,
+									A.PortfolioId 
+									
+						HAVING		SUM(C.TransactionAmount)>0
+	 					) AS S ON A.AccountId = S.AccountId
+	 					
+GROUP BY    A.PortfolioId
 
-SELECT PortfolioID 
-
-
--- Get Account Ids with Positive transaction amounts
-(
-SELECT 		A.AccountId, A.PortfolioId, SUM(C.TransactionAmount) AS TotalPayed
-FROM 		Account AS A
-			INNER JOIN Collections 	AS C ON A.AccountId = C.AccountId
-GROUP BY	A.AccountId, A.PortfolioId 
-HAVING		SUM(C.TransactionAmount)>0
-ORDER BY 	A.AccountId
-) AS M 
-
-
--- Get Account Ids with 0 total transaction amounts
-(
-SELECT		A.AccountId, SUM(C.TransactionAmount) AS TotalPayed
-FROM 		Account AS A
-			INNER JOIN Collections AS C ON A.AccountId = C.AccountId
-GROUP BY 	A.AccountId
-HAVING		SUM(C.TransactionAmount)=0
-			OR
-			ISNULL(1,C.TransactionAmount)=1
-ORDER BY 	A.AccountId
-) AS N
+ORDER BY	A.PortfolioId
 
 
 
-INNER JOIN Portfolio 	AS P ON A.PortfolioId = P.PortfolioId
 -- Q6: Which product type are people most likely to pay into?
 
 
 
--- Q7: What is the pay rate over the last 12 months? That is the number of customers who have paid in the last 12 months divided by the number of customers who 
---had not cleared their balance in the last 12 months.
+-- Q7: What is the pay rate over the last 12 months?
+-- That is the number of customers who have paid in the last 12 months divided by the number of customers who 
+-- had not cleared their balance in the last 12 months.
 
 
 
