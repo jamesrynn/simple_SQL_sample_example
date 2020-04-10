077aa02275b8a12886b23a885a379a1dd05b5927
James <jajrynn@googlemail.com>
2020-04-10 20:31:21 +0100

    added extra comments and column names to solutions

--- sol1.sql
+++ sol1.sql
@@ -4,8 +4,9 @@ Solutions to Part 1 of the Query Tasks.
 
 
 -- Q1: What is the average balance in each portfolio?
+---------------------------------------------------------------------------------------
 SELECT 		P.PortfolioName,
-			AVG(A.PurchaseBalance) 
+			AVG(A.PurchaseBalance) AS AvgBalance 
 			
 FROM 		Account AS A
 			INNER JOIN 	Portfolio as P ON P.PortfolioId = A.PortfolioId 
@@ -13,25 +14,31 @@ FROM 		Account AS A
 GROUP BY 	P.PortfolioName
 
 ORDER BY 	P.PortfolioName
+---------------------------------------------------------------------------------------
+
 
 
 
 -- Q2: What is the average balance by Product Type?
+---------------------------------------------------------------------------------------
 SELECT 		A.ProductType,
-			AVG(A.PurchaseBalance) 
+			AVG(A.PurchaseBalance) AS AvgBalance
 			
 FROM 		Account AS A
 
 GROUP BY 	A.ProductType 
 
 ORDER BY 	A.ProductType
+---------------------------------------------------------------------------------------
+
 
 
 
 -- Q3: What is the average balance today (hint: discount previous collections from purchase value) of all customers whose surname begins with a G?
+---------------------------------------------------------------------------------------
 SELECT DISTINCT 	Cu.LastName,
 					Cu.FirstName,
-					AVG(A.PurchaseBalance - Co.TransactionAmount) AS AverageBalance
+					AVG(A.PurchaseBalance - Co.TransactionAmount) AS AvgBalance
 					
 FROM 				Account as A
 					INNER JOIN Collections 		AS Co ON Co.AccountId = A.AccountId
@@ -46,16 +53,19 @@ GROUP BY 			Cu.LastName,
 					
 ORDER BY 			Cu.LastName,
 					Cu.FirstName
+---------------------------------------------------------------------------------------
 
+					
 
 
 -- Q4: Which customer has the highest number of accounts?
--- Solution 1: Count the number of accounts each customer has and list them in decreasing order. Read the answer from the top of the table that is returned.
+-- SOLUTION 1: Count the number of accounts each customer has and list them in decreasing order. Read the answer from the top of the table that is returned.
+---------------------------------------------------------------------------------------
 SELECT 		TOP 5
 			C.FirstName,
 			C.LastName,
 			AC.CustomerId,
-			COUNT(AC.AccountId) AS NumAcc
+			COUNT(AC.AccountId) AS NumAccounts
 			
 FROM 		AccountCustomer AS AC
 			INNER JOIN Customer AS C ON AC.CustomerId = C.CustomerId 
@@ -63,15 +73,20 @@ FROM 		AccountCustomer AS AC
 GROUP BY 	C.FirstName, C.LastName, AC.CustomerId
 
 ORDER BY 	COUNT(AC.AccountId) DESC
-
+---------------------------------------------------------------------------------------
 	
--- Solution 2: Count the number of accounts each customer has using a subquery and then retrievefrom that the maximum value of the results returned.
+
+
+
+-- Q4: Which customer has the highest number of accounts?
+-- SOLUTION 2: Count the number of accounts each customer has using a subquery and then retrievefrom that the maximum value of the results returned.
 -- Note that here we do not return the details of the customer/customers who has/have this number of accounts.
-SELECT 	MAX(S.NumAcc) AS MaxNAccounts
+---------------------------------------------------------------------------------------
+SELECT 	MAX(S.NumAcc) AS MaxNumAccounts
 
 FROM 	(
 		SELECT 		C.FirstName,
-					C.LastName
+					C.LastName,
 					AC.CustomerId,
 					COUNT(AC.AccountId) AS NumAcc
 					
@@ -82,7 +97,7 @@ FROM 	(
 					C.LastName,
 					AC.CustomerId
 		) AS S
-
+---------------------------------------------------------------------------------------
 	
 
 				
--- sol2.sql
+++ sol2.sql
@@ -5,6 +5,7 @@ Solutions to Part 2 of the Query Tasks.
 
 -- Q5: Which portfolio are people most likely to pay into?
 -- i.e., Number of people who have ever paid / number of people in portfolio.
+---------------------------------------------------------------------------------------
 SELECT 		A.PortfolioId,
 			COUNT(A.AccountId) AS NumAccounts,
 			AVG(
@@ -32,17 +33,49 @@ FROM 		Account AS A
 GROUP BY    A.PortfolioId
 
 ORDER BY	A.PortfolioId
+---------------------------------------------------------------------------------------
+
 
 
 
 -- Q6: Which product type are people most likely to pay into?
+---------------------------------------------------------------------------------------
+SELECT 		A.PortfolioId,
+			COUNT(A.AccountId) AS NumAccounts,
+			AVG(
+				CASE WHEN S.AccountId IS NOT NULL 
+       			THEN 1.0
+       			ELSE 0.0
+				END
+				) AS PayRate
+				
+FROM 		Account AS A
+			-- Accounts where payments have been made.
+			LEFT JOIN	(
+						SELECT 		A.AccountId,
+									A.PortfolioId
+									
+						FROM 		Account AS A
+									INNER JOIN Collections AS C ON A.AccountId = C.AccountId
+									
+						GROUP BY	A.AccountId,
+									A.PortfolioId 
+									
+						HAVING		SUM(C.TransactionAmount)>0
+	 					) AS S ON A.AccountId = S.AccountId
+	 					
+GROUP BY    A.PortfolioId
+
+ORDER BY	A.PortfolioId
+---------------------------------------------------------------------------------------
 
 
 
+---------------------------------------------------------------------------------------
 -- Q7: What is the pay rate over the last 12 months?
--- That is the number of customers who have paid in the last 12 months divided by the number of customers who 
+-- That is, the number of customers who have paid in the last 12 months divided by the number of customers who 
 -- had not cleared their balance in the last 12 months.
-
+---------------------------------------------------------------------------------------
 
 
 
